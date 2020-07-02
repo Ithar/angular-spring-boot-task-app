316e5ec30ce97709518f42bb1d42b14d4ec4cd89
ithar.malik <ithar.malik@bjss.com>
2020-07-02 08:07:11 +0100

    Add task model.

--- src/main/frontend/src/app/tasks/tasks-list/tasks-list.component.ts
+++ src/main/frontend/src/app/tasks/tasks-list/tasks-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Task} from "../task.model";
 
 @Component({
   selector: 'app-tasks-list',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TasksListComponent implements OnInit {
 
-  constructor() { }
+    tasks: Task[] = [];
 
-  ngOnInit(): void {
-  }
+    constructor() { }
+
+    ngOnInit(): void {
+        this.tasks.push(new Task(1, 'task1', '07/07/2020', false ));
+        this.tasks.push(new Task(2, 'task2', '07/07/2021', true ));
+        this.tasks.push(new Task(2, 'task3', '07/07/2023', false ));
+    }
+
+    onTaskChange(event, task) {
+        console.log("Boom");
+    }
 
 }
